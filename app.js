012943d7733e3a34c566e43ae3e9eff74e4f2e96
Vrganj <luka.barbic@hotmail.com>
2019-04-13 12:13:03 +0200

    I added a less shitty way to do show / hide

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-function showResponse(id) {
-    const el = document.getElementById(id);
-    el.style.display = el.style.display === 'none' ? '' : 'none';
+window.onload = (e) => {
+    const elements = document.getElementsByClassName('card');
+
+    for (let i = 0; i < elements.length; i++) {
+        const el = elements[i].getElementsByClassName('response')[0];
+        if (el == null) continue;
+        el.getElementsByTagName('button')[0]-addEventListener('click', (e) => {
+            el.getElementsByClassName('code')[0].style.display = el.getElementsByClassName('code')[0].style.display === 'none' ? '' : 'none';
+        })
+    }
 }
