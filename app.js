5ad2f2e575d869ff25957627eabfb7fa1f6f35ec
Vrganj <luka.barbic@hotmail.com>
2019-04-13 13:16:20 +0200

    testing

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ window.onload = (e) => {
 
     for (let i = 0; i < elements.length; i++) {
         const el = elements[i].getElementsByClassName('response')[0];
-        if (el != null) el.getElementsByTagName('button')[0].addEventListener('click', (e) => {
+        if (el != null) el.getElementsByTagName('button')[0].addEventListener('click', async (e) => {
+            el.getElementsByClassName('code')[0]
+            const rn = el.getElementsByClassName('code')[0].getAttribute('data-response');
+            const res = await fetch(`https://vrganj.github.io/Minehut-API-Documentation/responses/${rn}.txt`).then(res => res.text());
+            console.log(res);
             el.getElementsByClassName('code')[0].style.display = el.getElementsByClassName('code')[0].style.display === 'none' ? '' : 'none';
         })
     }
